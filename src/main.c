9839da01b316ef2f44ac3e445641bdca7e118622
Cem GEÇGEL <gecgelcem@outlook.com>
2023-09-28 21:47:02 +0300

    setup: Switch to C++

--- src/main.c
+++
@@ -1,5 +0,0 @@
-// Houses the entrypoint to the duru tool.
-
-#include <stdio.h>
-
-int main() { puts("Hello, Duru!"); }
---
+++ src/main.cxx
@@ -0,0 +1,7 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() noexcept {
+  std::cout << "Hello, Duru!" << std::endl;
+  return EXIT_SUCCESS;
+}
