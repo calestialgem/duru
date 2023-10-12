eac438dd2a259d3cb63dcf5c4d480e1ce9729680
Cem GEÇGEL <gecgelcem@outlook.com>
2023-10-12 13:48:14 +0300

    feat: Remove Arena

--- src/main.c
+++ src/main.c
@@ -1,21 +1,5 @@
 #include "duru.h"
 
 #include <stdio.h>
-#include <string.h>
 
-int main() {
-    DuruArena arena;
-    duruDoOrDie(duruCreateArena, &arena);
-    char const* staticMessage0 = "Repeat with me; ";
-    char const* staticMessage1 = "Hello, Duru!";
-    DuruSize    messageLength =
-      (DuruSize)strlen(staticMessage0) + (DuruSize)strlen(staticMessage1) + 1;
-    char* message;
-    duruDoOrDie(duruAllocateArray, &arena, char, messageLength, &message);
-    memcpy(message, staticMessage0, strlen(staticMessage0));
-    memcpy(
-      message + strlen(staticMessage0), staticMessage1, strlen(staticMessage1));
-    message[messageLength] = 0;
-    puts(message);
-    duruDoOrDie(duruDestroyArena, &arena);
-}
+int main() { puts("Hello, Duru!"); }
