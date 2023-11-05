83ad5ae9d3889b2c8c455b33eb01abb82e7ff229
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-05 19:17:01 +0300

    fix: Add Missing Documentation

--- duru/src/duru/MissingImplementationException.java
+++ duru/src/duru/MissingImplementationException.java
@@ -1,6 +1,7 @@
 package duru;
 
 import java.io.Serial;
+import java.io.Serializable;
 
 /**
  * Exception that is thrown when an implementation is missing. This is used for
@@ -9,6 +10,11 @@ import java.io.Serial;
  * incremental improvements.
  */
 final class MissingImplementationException extends RuntimeException {
+  /**
+   * Integer that identifies this type's instances when they are deserialized.
+   * Required for all {@link Exception}s because they implement
+   * {@link Serializable}.
+   */
   @Serial
   private static final long serialVersionUID = -7091900440604439526L;
 }
