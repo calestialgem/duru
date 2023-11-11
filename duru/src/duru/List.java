c8f6cfa2ac28316ebd27ebf6b5e13c62511b923b
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-11 11:10:11 +0300

    feat: Implement List

--- duru/src/duru/List.java
+++ duru/src/duru/List.java
@@ -1,3 +1,13 @@
 package duru;
 
-public record List<E>(E[] elements) implements ListLike {}
+public record List<E>(E[] elements) implements ListLike {
+  @Override
+  public int length() {
+    return elements.length;
+  }
+
+  @Override
+  public Object get(int index) {
+    return elements[index];
+  }
+}
