007e95423b6bd2b271b4e27ef120647fe581e0a2
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-11 11:25:11 +0300

    feat: Implement Set Buffer

--- duru/src/duru/SetBuffer.java
+++ duru/src/duru/SetBuffer.java
@@ -10,8 +10,22 @@ final class SetBuffer<M> implements SetLike<M> {
   }
 
   @Override
-  public int length() {}
+  public int length() {
+    return members.length();
+  }
 
   @Override
-  public boolean contains(M member) {}
+  public boolean contains(M member) {
+    var expectedBucket = member.hashCode();
+    for (var i = 0; i < buckets.length(); i++) {
+      var bucket = (expectedBucket + i) % buckets.length();
+      var index  = buckets.get(bucket);
+      if (index == -1)
+        break;
+      var mapped = members.get(index);
+      if (mapped.equals(member))
+        return true;
+    }
+    return false;
+  }
 }
