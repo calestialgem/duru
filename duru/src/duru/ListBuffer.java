a8c3bd2765ff6d84043c699970b972056c86e85d
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-11 11:24:00 +0300

    feat: Initializing Set Buffer

--- duru/src/duru/ListBuffer.java
+++ duru/src/duru/ListBuffer.java
@@ -4,6 +4,12 @@ final class ListBuffer<E> implements ListLike<E> {
   private E[] elements;
   private int length;
 
+  @SuppressWarnings("unchecked")
+  public ListBuffer() {
+    elements = (E[]) new Object[0];
+    length   = 0;
+  }
+
   @Override
   public int length() {
     return length;
--- duru/src/duru/SetBuffer.java
+++ duru/src/duru/SetBuffer.java
@@ -1,3 +1,17 @@
 package duru;
 
-final class SetBuffer<M> implements SetLike<M> {}
+final class SetBuffer<M> implements SetLike<M> {
+  private final ListBuffer<M>       members;
+  private final ListBuffer<Integer> buckets;
+
+  public SetBuffer() {
+    this.members = new ListBuffer<>();
+    this.buckets = new ListBuffer<>();
+  }
+
+  @Override
+  public int length() {}
+
+  @Override
+  public boolean contains(M member) {}
+}
