11d49dd7ed55013591a306d422519a644816b6ef
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-11 11:11:04 +0300

    feat: Implement List Buffer

--- duru/src/duru/ListBuffer.java
+++ duru/src/duru/ListBuffer.java
@@ -1,3 +1,16 @@
 package duru;
 
-final class ListBuffer<E> implements ListLike<E> {}
+final class ListBuffer<E> implements ListLike<E> {
+  private E[] elements;
+  private int length;
+
+  @Override
+  public int length() {
+    return length;
+  }
+
+  @Override
+  public E get(int index) {
+    return elements[index];
+  }
+}
