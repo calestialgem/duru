e89bdf66bc38877e93267fe57607bcd8b7d6c05d
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 14:04:40 +0300

    feat: Implement Recreate

--- duru/src/duru/Persistance.java
+++ duru/src/duru/Persistance.java
@@ -1,9 +1,20 @@
 package duru;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class Persistance {
   public static void recreate(Path directory) {
+    if (Files.exists(directory))
+      delete(directory);
+    create(directory);
+  }
+
+  private static void delete(Path directory) {
+    throw new RuntimeException("Unimplemented!");
+  }
+
+  private static void create(Path directory) {
     throw new RuntimeException("Unimplemented!");
   }
 
