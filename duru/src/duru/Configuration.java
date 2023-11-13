bf42482101bdb62afa433e3c4a69e42312276ccd
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 23:28:18 +0300

    feat: Implement Configuration Structure

--- duru/src/duru/Configuration.java
+++ duru/src/duru/Configuration.java
@@ -1,7 +1,13 @@
 package duru;
 
+import java.util.List;
+
 public sealed interface Configuration {
-  record Project() implements Configuration {}
+  record Project(String name, List<Package> executables)
+    implements Configuration
+  {}
+
+  record Package(String name) implements Configuration {}
 
   String name = "project.duru";
 }
