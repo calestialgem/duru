854dbce16e3e5ccb504a8442287577b616bfae15
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 14:29:31 +0300

    feat: Check Initialization Inside Another Project

--- duru/src/duru/Initializer.java
+++ duru/src/duru/Initializer.java
@@ -1,9 +1,18 @@
 package duru;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class Initializer {
   public static void initialize(Path directory) {
+    for (var i = directory; i != null; i = i.getParent()) {
+      var config = i.resolve("project.duru");
+      if (Files.exists(config)) {
+        throw new RuntimeException(
+          "%s: error: Could not initialize inside another project, which is configured at `%s`!"
+            .formatted(directory, config));
+      }
+    }
     throw Exceptions.unimplemented();
   }
 
