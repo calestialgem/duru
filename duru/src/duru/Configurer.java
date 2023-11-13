a0eb6c56dea42f7074279e981e5990df47beffd9
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 23:59:11 +0300

    feat: Configurer Context

--- duru/src/duru/Configurer.java
+++ duru/src/duru/Configurer.java
@@ -1,12 +1,33 @@
 package duru;
 
 import java.nio.file.Path;
+import java.util.Optional;
 
 public final class Configurer {
   public static Configuration.Project configure(Path file) {
-    var text = Persistance.read(file);
-    throw Diagnostic.error("unimplemented");
+    var configurer = new Configurer(file);
+    return configurer.configure();
   }
 
-  private Configurer() {}
+  private final Path                           file;
+  private Optional<Configuration.Identifier>   name;
+  private ListBuffer<Configuration.Executable> executables;
+  private String                               text;
+  private int                                  index;
+
+  private Configurer(Path file) {
+    this.file = file;
+  }
+
+  private Configuration.Project configure() {
+    text        = Persistance.read(file);
+    name        = Optional.empty();
+    executables = ListBuffer.create();
+    index       = 0;
+    return new Configuration.Project(
+      -1,
+      index,
+      name.get(),
+      executables.toList());
+  }
 }
