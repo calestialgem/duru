edcec2b9e9b58455864ca84edf3f0246441d5fbf
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 14:09:04 +0300

    feat: Implement Delete

--- duru/src/duru/Persistance.java
+++ duru/src/duru/Persistance.java
@@ -1,7 +1,11 @@
 package duru;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public final class Persistance {
   public static void recreate(Path directory) {
@@ -11,7 +15,38 @@ public final class Persistance {
   }
 
   private static void delete(Path directory) {
-    throw Exceptions.unimplemented();
+    try {
+      Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+        @Override
+        public FileVisitResult visitFile(
+          Path file,
+          BasicFileAttributes attributes)
+          throws IOException
+        {
+          Files.delete(file);
+          return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(
+          Path directory,
+          IOException cause)
+          throws IOException
+        {
+          if (cause != null) {
+            throw cause;
+          }
+          Files.delete(directory);
+          return FileVisitResult.CONTINUE;
+        }
+      });
+    }
+    catch (IOException cause) {
+      throw new RuntimeException(
+        "%s: failure: Could not delete the directory!"
+          .formatted(directory.toAbsolutePath().normalize()),
+        cause);
+    }
   }
 
   private static void create(Path directory) {
