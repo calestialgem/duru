fadf44da83c8641131847ae92691cf54af7e61f6
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 13:29:29 +0300

    feat: Initialization Signatures

--- duru.cxx
+++ duru.cxx
@@ -1,6 +1,24 @@
 #include <cstdio>
 #include <cstdlib>
 
+namespace duru {
+  struct Unit {};
+
+  template<typename Value, typename Error>
+  struct Result {};
+
+  struct Arena {};
+
+  struct String {};
+
+  struct InitializationError {};
+
+  Result<Unit, InitializationError> initialize(
+      Arena& temporary, String directory, String name) {
+    return {};
+  }
+}
+
 int main() {
   std::puts("Hello, World!");
   return EXIT_SUCCESS;
