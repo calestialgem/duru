31f69a3af4bb59d4fa2a5ce2a481ca458fe6eedd
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 14:10:17 +0300

    feat: Exception Titles

--- duru/src/duru/Exceptions.java
+++ duru/src/duru/Exceptions.java
@@ -2,7 +2,7 @@ package duru;
 
 public final class Exceptions {
   public static RuntimeException unimplemented() {
-    throw new RuntimeException("Unimplemented!");
+    throw new RuntimeException("fatal error: Unimplemented!");
   }
 
   private Exceptions() {}
