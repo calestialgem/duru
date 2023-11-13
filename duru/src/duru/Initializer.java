91185e103c85186aaf86e96f5a8dfee985c20887
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-13 17:30:20 +0300

    feat: Implement Checking Name

--- duru/src/duru/Initializer.java
+++ duru/src/duru/Initializer.java
@@ -23,7 +23,30 @@ public final class Initializer {
   }
 
   private static void checkIdentifier(String name) {
-    throw Exceptions.unimplemented();
+    for (var i = 0; i != name.length(); i = name.offsetByCodePoints(i, 1)) {
+      var character = name.codePointAt(i);
+      if (!Character.isLetterOrDigit(character))
+        throw new RuntimeException(
+          "error: Name `%s` has a non-letter or digit character `%c` at %d!"
+            .formatted(name, character, i));
+    }
+    if (name.length() == 0)
+      throw new RuntimeException("error: Name is empty!");
+    var initial = name.codePointAt(0);
+    if (!Character.isLetter(initial))
+      throw new RuntimeException(
+        "error: Name `%s` starts with a non-letter character `%c`!"
+          .formatted(name, initial));
+    switch (name) {
+      case "project", "executable" ->
+        throw new RuntimeException(
+          "error: Name `%s` is a reserved word for project configuration!"
+            .formatted(name));
+      case "void" ->
+        throw new RuntimeException(
+          "error: Name `%s` is a reserved word!".formatted(name));
+      default -> {}
+    }
   }
 
   private Initializer() {}
