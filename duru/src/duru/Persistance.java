36295631da8b000e1bf7b707cc03beda528d822d
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-14 18:09:14 +0300

    feat: Implement Recreate and Delete

--- duru/src/duru/Persistance.java
+++ duru/src/duru/Persistance.java
@@ -1,6 +1,11 @@
 package duru;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public final class Persistance {
   public static Path path(String path) {
@@ -8,6 +13,47 @@ public final class Persistance {
   }
 
   public static void recreate(Path directory) {
+    if (Files.exists(directory))
+      delete(directory);
+    create(directory);
+  }
+
+  private static void delete(Path directory) {
+    try {
+      Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+        @Override
+        public FileVisitResult visitFile(
+          Path file,
+          BasicFileAttributes attributes)
+          throws IOException
+        {
+          Files.delete(file);
+          return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(
+          Path directory,
+          IOException exception)
+          throws IOException
+        {
+          if (exception != null) {
+            throw exception;
+          }
+          Files.delete(directory);
+          return FileVisitResult.CONTINUE;
+        }
+      });
+    }
+    catch (IOException cause) {
+      throw Subject
+        .get()
+        .diagnose("failure", "could not delete `%s`", directory)
+        .exception(cause);
+    }
+  }
+
+  private static void create(Path directory) {
     throw Subject.get().diagnose("failure", "unimplemented").exception();
   }
 
