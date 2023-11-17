2d2d29760c104eaf06220f82325639e1f5dc09ff
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-17 17:00:05 +0300

    feat: Lexing Configuration

--- duru/src/duru/ConfigurationLexer.java
+++ duru/src/duru/ConfigurationLexer.java
@@ -2,8 +2,101 @@ package duru;
 
 public final class ConfigurationLexer {
   public static List<ConfigurationToken> lex(Source source) {
-    throw Subject.unimplemented();
+    var lexer = new ConfigurationLexer(source);
+    return lexer.lex();
   }
 
-  private ConfigurationLexer() {}
+  private final Source                   source;
+  private ListBuffer<ConfigurationToken> tokens;
+  private int                            index;
+  private int                            begin;
+  private int                            initial;
+
+  private ConfigurationLexer(Source source) {
+    this.source = source;
+  }
+
+  private List<ConfigurationToken> lex() {
+    tokens = ListBuffer.create();
+    index  = 0;
+    while (hasCharacter()) {
+      begin   = index;
+      initial = getCharacter();
+      advance();
+      switch (initial) {
+        case ' ', '\r', '\n' -> {}
+        case '/' -> {
+          if (hasCharacter() && getCharacter() == '/') {
+            advance();
+            while (hasCharacter() && getCharacter() != '\n')
+              advance();
+            break;
+          }
+          if (!hasCharacter() || getCharacter() != '*') {
+            throw Subject.error("incomplete comment");
+          }
+          advance();
+          var blockComments = 1;
+          while (hasCharacter()) {
+            var character = getCharacter();
+            advance();
+            if (character == '*' && hasCharacter() && getCharacter() == '/') {
+              advance();
+              blockComments--;
+              if (blockComments == 0)
+                break;
+            }
+            if (character == '/' && hasCharacter() && getCharacter() == '*') {
+              advance();
+              blockComments++;
+            }
+          }
+          if (blockComments != 0) {
+            throw Subject.error("incomplete block comment");
+          }
+        }
+        case '{' -> tokens.add(new ConfigurationToken.OpeningBrace(location()));
+        case '}' -> tokens.add(new ConfigurationToken.ClosingBrace(location()));
+        case ';' -> tokens.add(new ConfigurationToken.Semicolon(location()));
+        case '.' -> tokens.add(new ConfigurationToken.Dot(location()));
+        default -> {
+          if (Text.isIdentifierInitial(initial)) {
+            while (hasCharacter() && Text.isIdentifierBody(getCharacter())) {
+              advance();
+            }
+            var text = source.contents().substring(begin, index);
+            switch (text) {
+              case "module" ->
+                tokens.add(new ConfigurationToken.Module(location()));
+              case "executable" ->
+                tokens.add(new ConfigurationToken.Executable(location()));
+              case "exports" ->
+                tokens.add(new ConfigurationToken.Exports(location()));
+              default ->
+                tokens.add(new ConfigurationToken.Identifier(location(), text));
+            }
+            break;
+          }
+          throw Subject.error("unknown character `%c`", initial);
+        }
+      }
+    }
+    return tokens.toList();
+  }
+
+  private Location location() {
+    return new Location(source, begin, index);
+  }
+
+  private boolean hasCharacter() {
+    return index != source.contents().length();
+  }
+
+  private int getCharacter() {
+    return source.contents().codePointAt(index);
+  }
+
+  private void advance() {
+    index = source.contents().offsetByCodePoints(index, 1);
+  }
 }
