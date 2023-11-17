53bf11c246ce1696cc6d0683b87c74cf86aad6ab
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-17 19:29:28 +0300

    refactor: Optional without Dependencies

--- duru/src/duru/Optional.java
+++ duru/src/duru/Optional.java
@@ -1,7 +1,5 @@
 package duru;
 
-import java.util.Objects;
-
 public record Optional<Value>(Value value) implements Collection<Value> {
   public static <Value> Optional<Value> present(Value value) {
     return new Optional<>(value);
@@ -22,21 +20,13 @@ public record Optional<Value>(Value value) implements Collection<Value> {
   }
 
   @Override
-  public int hashCode() {
-    return Objects.hashCode(value);
-  }
-
-  @Override
-  public boolean equals(Object other) {
-    if (this == other)
-      return true;
-    if (!(other instanceof Optional otherOptional))
-      return false;
-    return Objects.equals(value, otherOptional.value);
-  }
-
-  @Override
   public String toString() {
-    return Objects.toString(value);
+    var string = new StringBuilder();
+    string.append('[');
+    if (!isEmpty()) {
+      string.append(getFirst());
+    }
+    string.append(']');
+    return string.toString();
   }
 }
