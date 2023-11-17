9341dafa43f7e83e903ac403c49e45976987d957
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-17 17:00:46 +0300

    feat: Override Object Methods

--- duru/src/duru/List.java
+++ duru/src/duru/List.java
@@ -18,4 +18,23 @@ public final class List<Element> {
   private List(Element[] elements) {
     this.elements = elements;
   }
+
+  @Override
+  public int hashCode() {
+    return Arrays.deepHashCode(elements);
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (this == other)
+      return true;
+    if (!(other instanceof List otherList))
+      return false;
+    return Arrays.deepEquals(elements, otherList.elements);
+  }
+
+  @Override
+  public String toString() {
+    return Arrays.toString(elements);
+  }
 }
