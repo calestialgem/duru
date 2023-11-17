71072e460b8ba56dfddde4ae54c94bcb24dac0c0
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-17 15:06:45 +0300

    feat: Expand Semantic Representation

--- duru/src/duru/Semantic.java
+++ duru/src/duru/Semantic.java
@@ -17,35 +17,111 @@ public sealed interface Semantic {
   {}
 
   sealed interface Symbol extends Semantic {
+    String packageName();
     boolean isPublic();
     String name();
   }
 
-  record Function(
+  sealed interface Builtin extends Symbol {
+    @Override
+    default String packageName() {
+      return "duru";
+    }
+
+    @Override
+    default boolean isPublic() {
+      return true;
+    }
+  }
+
+  sealed interface Type extends Semantic {}
+
+  record Struct(String packageName, boolean isPublic, String name)
+    implements Type, Symbol
+  {}
+
+  record Byte() implements Type, Builtin {
+    @Override
+    public String name() {
+      return "Byte";
+    }
+  }
+
+  record Natural32() implements Type, Builtin {
+    @Override
+    public String name() {
+      return "Natural32";
+    }
+  }
+
+  record Integer32() implements Type, Builtin {
+    @Override
+    public String name() {
+      return "Integer32";
+    }
+  }
+
+  record Noreturn() implements Type, Builtin {
+    @Override
+    public String name() {
+      return "Noreturn";
+    }
+  }
+
+  record Pointer(Type pointee) implements Type {}
+
+  sealed interface Procedure extends Semantic {
+    Map<String, Type> parameters();
+    Optional<Type> returnType();
+  }
+
+  record Proc(
+    String packageName,
     boolean isPublic,
-    Type returnType,
     String name,
     Map<String, Type> parameters,
-    Statement body) implements Symbol
+    Optional<Type> returnType,
+    Statement body) implements Procedure, Symbol
+  {}
+
+  record ExternalProc(
+    String packageName,
+    boolean isPublic,
+    String name,
+    Map<String, Type> parameters,
+    Optional<Type> returnType,
+    String externalName) implements Procedure, Symbol
   {}
 
   sealed interface Statement extends Semantic {}
 
   record Block(List<Statement> innerStatements) implements Statement {}
 
+  record If(
+    Expression condition,
+    Statement trueBranch,
+    Optional<Statement> falseBranch) implements Statement
+  {}
+
+  record Return(Optional<Expression> value) implements Statement {}
+
+  record Var(String name, Optional<Type> type, Expression initialValue)
+    implements Statement
+  {}
+
   record Discard(Expression discarded) implements Statement {}
 
   sealed interface Expression extends Semantic {}
 
-  record Invocation(Function callee, List<Expression> arguments)
+  record LessThan(Expression left, Expression right) implements Expression {}
+
+  record Invocation(String packageName, String name, List<Expression> arguments)
     implements Expression
   {}
 
+  record NaturalConstant(long value) implements Expression {}
+
   record StringConstant(String value) implements Expression {}
 
-  sealed interface Type extends Semantic {}
-
-  record Void() implements Type {}
-
-  record ConstantString() implements Type {}
+  record LocalAccess(String name) implements Expression {}
 }
