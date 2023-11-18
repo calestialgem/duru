9ae09bbc3125dbe5512a80a83986c44a4e740aa5
Cem GEÇGEL <gecgelcem@outlook.com>
2023-11-18 18:21:40 +0300

    feat: Resolving Packages

--- duru/src/duru/PackageChecker.java
+++ duru/src/duru/PackageChecker.java
@@ -23,26 +23,9 @@ public final class PackageChecker {
   }
 
   private Semantic.Package check() {
-    var directory = sources;
-    var index     = name.indexOf('.');
-    while (index != -1) {
-      var next = name.indexOf('.', index + 1);
-      if (next == -1) {
-        directory = directory.resolve(name.substring(index + 1));
-      }
-      else {
-        directory = directory.resolve(name.substring(index + 1, next));
-      }
-      index = next;
-    }
-    var symbols = AcyclicCache.<String, Semantic.Symbol>create(null);
-    for (var file : Persistance.list(directory)) {
-      var source       = new Source(file, Persistance.load(file));
-      var fullFilename = file.getFileName().toString();
-      var filename     =
-        fullFilename.substring(0, fullFilename.length() - ".duru".length());
-      Persistance.record(artifacts, source, name, filename, "source");
-    }
+    var symbols    = AcyclicCache.<String, Semantic.Symbol>create(null);
+    var resolution = PackageResolver.resolve(sources, artifacts, name);
+    Persistance.record(artifacts, resolution, name, "resolution");
     throw Subject.unimplemented();
   }
 }
